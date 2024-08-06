98936a8937d76632cf8e3acee827ade9668c8d4b
Jeon GaHyun <119282067+weweGH@users.noreply.github.com>
2024-08-06 17:56:05 +0900

    [level 2] Title: 올바른 괄호, Time: 7.35 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,11 @@
+def solution(s):
+    stack = []
+    for char in s:
+        if char == '(':
+            stack.append(char)
+        elif char == ')':
+            if not stack:
+                return False
+            stack.pop()
+    return len(stack) == 0
+
